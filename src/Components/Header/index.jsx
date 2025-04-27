46df6f343d83546f2738dbdc3fa335834f8bf2f0
Guilherme Reis <guireis1164@gmail.com>
2025-04-27 08:38:19 -0300

    Refactor(React/Header): Componentizes the Header with subcomponents for better modularity

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,30 +5,46 @@ import styles from "./style.module.css"
 export default function Header({ currentPage }) {
   return (
     <header className={styles.Header}>
-      <div className={styles.DivLogo}>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className={`${styles.logo} ${styles.react}`} alt="React logo" />
-        </a>
-      </div>
+      <ContentLogoNav />
 
       <h1 className={styles.TitlePage}>Vite + React {currentPage}</h1>
 
-      <div className={styles.nav}>
-        <nav>
-          <a className={styles.NavPages} href="/" rel="noopener noreferrer">
-            <i className="fi fi-rr-angle-left"></i>
-          </a>
-          <a className={styles.NavPages} href="/" rel="noopener noreferrer">
-            <ion-icon name="home"></ion-icon>
-          </a>
-          <a className={styles.NavPages} href="/" rel="noopener noreferrer">
-            <i className="fi fi-rr-angle-right"></i>
-          </a>
-        </nav>
-      </div>
+      <ContentNavPages />
     </header>
+  )
+}
+
+export function ContentLogoNav() {
+  return (
+    <div className={styles.DivLogo}>
+      <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+        <img src={viteLogo} className={styles.logo} alt="Vite logo" />
+      </a>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <img
+          src={reactLogo}
+          className={`${styles.logo} ${styles.react}`}
+          alt="React logo"
+        />
+      </a>
+    </div>
+  )
+}
+
+export function ContentNavPages() {
+  return (
+    <div className={styles.nav}>
+      <nav>
+        <a className={styles.NavPages} href="/" rel="noopener noreferrer">
+          <i className="fi fi-rr-angle-left"></i>
+        </a>
+        <a className={styles.NavPages} href="/" rel="noopener noreferrer">
+          <ion-icon name="home"></ion-icon>
+        </a>
+        <a className={styles.NavPages} href="/" rel="noopener noreferrer">
+          <i className="fi fi-rr-angle-right"></i>
+        </a>
+      </nav>
+    </div>
   )
 }
