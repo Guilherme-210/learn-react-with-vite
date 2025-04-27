e8d55f5c5902245c0715cf18894d3d228d302edd
Guilherme Reis <guireis1164@gmail.com>
2025-04-26 23:53:30 -0300

    Refactor(Styles): Modularizes Header styles to use CSS Modules

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,30 +1,30 @@
 import reactLogo from "../../assets/react.svg"
 import viteLogo from "/vite.svg"
-import "./style.css"
+import styles from "./style.module.css"
 
 export default function Header({ currentPage }) {
   return (
-    <header className="Header">
-      <div className="div-logo">
+    <header className={styles.Header}>
+      <div className={styles.DivLogo}>
         <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className={`${styles.logo} ${styles.react}`} alt="React logo" />
         </a>
       </div>
 
-      <h1 className="titlePage">Vite + React {currentPage}</h1>
+      <h1 className={styles.TitlePage}>Vite + React {currentPage}</h1>
 
-      <div className="nav">
+      <div className={styles.nav}>
         <nav>
-          <a className="nav_pages" href="/" rel="noopener noreferrer">
+          <a className={styles.NavPages} href="/" rel="noopener noreferrer">
             <i className="fi fi-rr-angle-left"></i>
           </a>
-          <a className="nav_pages" href="/" rel="noopener noreferrer">
+          <a className={styles.NavPages} href="/" rel="noopener noreferrer">
             <ion-icon name="home"></ion-icon>
           </a>
-          <a className="nav_pages" href="/" rel="noopener noreferrer">
+          <a className={styles.NavPages} href="/" rel="noopener noreferrer">
             <i className="fi fi-rr-angle-right"></i>
           </a>
         </nav>
