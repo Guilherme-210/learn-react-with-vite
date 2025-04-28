dfecfcb628481a897e0e87c17e7ddba4ef4cc6ef
Guilherme Reis <guireis1164@gmail.com>
2025-04-28 12:51:55 -0300

    Feat(buttonFollow): Add state management with useState

--- src/Components/ComponentsButtons/ButtonFollow/index.jsx
+++ src/Components/ComponentsButtons/ButtonFollow/index.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react"
 import styles from "./style.module.css"
 
-export default function ButtonFollow({ Content, Id, Title, Type, ClassName, NameVal }) {
+export default function ButtonFollow({
+  Content,
+  Id,
+  Title,
+  Type,
+  ClassName,
+  NameVal,
+}) {
   const finalClassName = styles[ClassName]
+  const [followText, setFollowText] = useState(Content)
 
   function follow() {
-    console.log(`Voce agora esta seguindo ${NameVal}`)
+    if (followText == "Seguir") {
+      console.log(`Voce esta seguindo ${NameVal}`)
+      setFollowText("Seguindo")
+    } else {
+      console.log(`Voce deixou de seguir ${NameVal}`)
+      setFollowText("Seguir")
+    }
   }
 
   return (
-    <button
-      className={finalClassName}
-      title={Title}
-      id={Id}
-      type={Type}
-      onClick={follow}
-    >
-      {Content}
+    <button className={finalClassName} title={Title} id={Id} type={Type} onClick={follow} >
+      {followText}
     </button>
   )
 }
