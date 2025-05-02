e6ea50a3fa7f64cc189df44dc0b8a747a100f387
Guilherme Reis <guireis1164@gmail.com>
2025-05-02 10:24:41 -0300

    Chore(GameLibrary): Remove dados de teste da biblioteca de jogos

--- src/pages/Exercicios/GameLibrary/index.jsx
+++ src/pages/Exercicios/GameLibrary/index.jsx
@@ -13,27 +13,8 @@ export default function GameLibrary() {
     const storageGames = localStorage.getItem("obc-game-lib")
     if (!storageGames) return []
     return JSON.parse(storageGames)
-  }, [
-    {
-      id: 1746143914588,
-      title: "Gog of War Ragnarok",
-      urlCover:
-        "https://m.media-amazon.com/images/I/8136lnf0n2L._AC_UF1000,1000_QL80_.jpg",
-      textAlt: "Capa do jogo God of War Ragnarok",
-      descripition:
-        "Foi desenvolvido pela Santa Monica Studio e publicado pela Sony Interactive Entertainment. Foi lançado em 9 de novembro de 2022 para PlayStation 4 e PlayStation 5. ",
-      themes: ["Ação", "Aventura", "Terror", "RPG", "Corrida", "Esportes"],
-    },
-    {
-      id: 1746143918402,
-      title: "Dying Light",
-      urlCover:
-        "https://upload.wikimedia.org/wikipedia/pt/4/4c/Dying_Light_jogo_capa.jpg?20220211113247",
-      textAlt: "Capa do jogo God of War Ragnarok",
-      descripition: `Dying Light é um jogo eletrônico de survival horror onde se retrata um apocalipse zumbi. Foi produzido pela Techland e publicado pela Warner Bros. Interactive Entertainment, seu anúncio aconteceu em maio de 2013, e eventualmente foi lançado em janeiro de 2015.`,
-      themes: ["Ação", "Aventura", "Terror", "RPG", "Corrida", "Esportes"],
-    },
-  ])
+  }
+)
 
   const removeGame = (id) => {
     setGameLibrary((state) => {
